6091c74fe2c9688fe7be591c2b50ac0999a8b8d0
AdolfoMR <adolfomendozaribera400@gmail.com>
2025-12-03 09:42:19 -0400

    Agregue y modifique el encabezado

--- addons/jah_documents/__manifest__.py
+++ addons/jah_documents/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'JAH-Mejoras Documentos de Venta',
+    'name': 'JAH-Mejoras Documentos de Venta 2',
     'version': '17.0.1.0.0',
     'category': 'Sales',
     'summary': 'Personalización de documentos de venta para JAH',
@@ -21,21 +21,33 @@
     'website': 'https://jah.com',
     'depends': [
         'base',
+        'account',
         'sale',
         'sale_management',
         'web',
     ],
     'data': [
+        #'reports/report_jah_external_layout.xml',
+        #'reports/report_jah_common_header.xml',
+        'reports/report_jah_external_layout.xml',
         'reports/report_jah_saleorder_document.xml',
+        'reports/report_jah_account_document.xml',
+        #'reports/report_jah_stock_document.xml',
     ],
+   
+    #'assets': {
+    #    'web.report_assets_common': [
+    #        'jah_sale_documents/static/src/css/report_styles.css',
+    #    ],
     
-    """
-    'assets': {
-        'web.report_assets_common': [
-            'jah_sale_documents/static/src/css/report_styles.css',
-        ],
-    },
-    """
+    
+    #'assets': {
+    #    'web.report_assets_common': [
+    #        'jah_sale_documents/static/src/css/jah_reports.css',
+    #    ],
+    #},
+    
+    
     
     'installable': True,
     'application': False,
